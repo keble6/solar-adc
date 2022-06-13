22f00624b7447bd7cc034f79e0bff96abac08dfe
Rob Wilson <rob@rwilson.org.uk>
2022-06-13 10:39:39 +0100

    Added averaging

--- main.ts
+++ main.ts
@@ -4,24 +4,48 @@ function readTime () {
     dateTime = "" + date + " " + time
 }
 function makeReading () {
-    basic.pause(1000)
-    // Scaled for ADC = 1023 at 3.3V on P0 and external resistors 270k to Vin and 330k to Gnd
-    Vin0 = pins.analogReadPin(AnalogPin.P0) * (6 / 1023)
-    // Round to 1 decimal place
-    Vin0 = Math.round(Vin0 * 100)
-    Vin0 = Vin0 / 100
-    Vin1 = pins.analogReadPin(AnalogPin.P1) * (6 / 1023)
-    Vin1 = Math.round(Vin1 * 100)
-    Vin1 = Vin1 / 100
-    Vin2 = pins.analogReadPin(AnalogPin.P2) * (6 / 1023)
-    Vin2 = Math.round(Vin2 * 100)
-    Vin2 = Vin2 / 100
+    // Read each ADC channel, generate average, then store in Vreadings array
+    for (let channel = 0; channel <= 2; channel++) {
+        if (channel == 0) {
+            Ntotal = 0
+            for (let sample = 0; sample <= sampleSize - 1; sample++) {
+                // Add each sample
+                Ntotal = Ntotal + pins.analogReadPin(AnalogPin.P0)
+            }
+        } else if (channel == 1) {
+            Ntotal = 0
+            for (let sample = 0; sample <= sampleSize - 1; sample++) {
+                // Add each sample
+                Ntotal = Ntotal + pins.analogReadPin(AnalogPin.P1)
+            }
+        } else {
+            Ntotal = 0
+            for (let sample = 0; sample <= sampleSize - 1; sample++) {
+                // Add each sample
+                Ntotal = Ntotal + pins.analogReadPin(AnalogPin.P2)
+            }
+        }
+        // generate average reading
+        Nave = Ntotal / sampleSize
+        // Scaled voltage: for ADC = 1023 at 3.3V on P0 and external resistors 270k to Vin and 330k to Gnd
+        Vscaled = Nave * (6 / 1023)
+        // Round to 2 decimal place2
+        Vscaled = Math.round(Vscaled * 100)
+        Vscaled = Vscaled / 100
+        if (channel == 0) {
+            Vreading = "" + convertToText(Vscaled) + ","
+        } else if (channel == 1) {
+            Vreading = "" + Vreading + convertToText(Vscaled) + ","
+        } else {
+            Vreading = "" + Vreading + convertToText(Vscaled)
+        }
+    }
 }
 // Test block
 input.onButtonPressed(Button.A, function () {
     if (count > 0) {
         basic.showString("" + (dateTimeReadings[count - 1]))
-        basic.showString("" + (batteryReadings[count - 1]))
+        basic.showString("" + (Vreadings[count - 1]))
         basic.pause(1000)
         basic.showNumber(count)
         basic.pause(1000)
@@ -53,7 +77,7 @@ function setDate () {
 }
 // Reset readings
 input.onButtonPressed(Button.AB, function () {
-    batteryReadings = []
+    Vreadings = []
     dateTimeReadings = []
     count = 0
     // Debug - reset
@@ -84,7 +108,7 @@ radio.onReceivedString(function (receivedString) {
         basic.pause(2000)
         for (let index = 0; index <= count - 1; index++) {
             radio.sendString("" + dateTimeReadings[index] + ",")
-            radio.sendString("" + (batteryReadings[index]))
+            radio.sendString("" + (Vreadings[index]))
             basic.pause(500)
         }
     }
@@ -92,8 +116,7 @@ radio.onReceivedString(function (receivedString) {
 // Show all 3 input voltages
 input.onButtonPressed(Button.B, function () {
     makeReading()
-    Vin = "" + Vin0 + "," + Vin1 + "," + Vin2
-    basic.showString(Vin)
+    basic.showString(Vreading)
 })
 serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
     stringIn = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
@@ -106,38 +129,46 @@ serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () 
     }
 })
 let command = ""
-let Vin = ""
 let minute = ""
 let hour = ""
 let year = ""
 let month = ""
 let stringIn = ""
-let Vin2 = 0
-let Vin1 = 0
-let Vin0 = 0
+let Vreading = ""
+let Vscaled = 0
+let Nave = 0
+let Ntotal = 0
 let dateTime = ""
 let time = ""
 let date = ""
 let count = 0
 let dateTimeReadings: string[] = []
-let batteryReadings: string[] = []
+let Vreadings: string[] = []
+let sampleSize = 0
+// Set the number of readings to get the average
+sampleSize = 2
+// Sampling time interval
 let oneMinute = 60000
-batteryReadings = []
+// This is the stored array of ADC voltage readings
+Vreadings = []
+// The date and time text string of the reading
 dateTimeReadings = []
+// The number of stored readings
 count = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
-// Debug - start serial
-serial.writeLine("abc")
 loops.everyInterval(oneMinute, function () {
     // Take readings once per hour
     if (DS3231.minute() == 0) {
+        let Vin2 = 0
+        let Vin1 = 0
+        let Vin0 = 0
         // Debug - make a reading
         serial.writeLine("Making a reading")
         readTime()
         dateTimeReadings.push(dateTime)
         makeReading()
-        batteryReadings.push("" + Vin0 + "," + Vin1 + "," + Vin2)
+        Vreadings.push("" + Vin0 + "," + Vin1 + "," + Vin2)
         count += 1
     }
     basic.showLeds(`
